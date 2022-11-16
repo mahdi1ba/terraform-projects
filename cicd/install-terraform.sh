0c6a68566a990b90c955ffe2ce998af63f3f476c
mahdibenayed2 <47032423+mahdibenayed2@users.noreply.github.com>
2022-11-16 16:30:18 +0100

    Update install-terraform.sh

--- cicd/install-terraform.sh
+++ cicd/install-terraform.sh
@@ -20,4 +20,4 @@ sudo yum install wget unzip -y
 sudo yum update -y
 sudo wget https://releases.hashicorp.com/terraform/0.12.17/terraform_0.12.17_linux_amd64.zip
 sudo unzip terraform_0.12.17_linux_amd64.zip -d /usr/local/bin/
-terraform --version
+terraform -v
