204615aaed3dbe0f1d5cfc5c23e811022a29f405
mahdibenayed2 <47032423+mahdibenayed2@users.noreply.github.com>
2022-11-16 16:43:24 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,7 @@ provider "aws" {
 # store the terraform state file in s3
 terraform {
   backend "s3" {
-    bucket  = "terrfaorm-starte-bucket-51418"
+    bucket  = "terrfaorm-starte-bucket-78179"
     key     = "build/terraform.tfstate"
     region  = "us-east-1"
     profile = "terraform-user"
