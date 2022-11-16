9500bcf342e35cdc167f1a717fbc50ff44ce3a8d
mahdibenayed2 <47032423+mahdibenayed2@users.noreply.github.com>
2022-11-16 15:28:28 +0100

    Update install-terraform.sh

--- cicd/install-terraform.sh
+++ cicd/install-terraform.sh
@@ -4,15 +4,15 @@
 set -eu
 su -
 # install yum-config-manager to manage your repositories
-sudo yum list updates -d3
-sudo yum update -y
-sudo yum install -y yum-utils
+yum list updates -d3
+yum update -y
+yum install -y yum-utils
 
 # use yum-config-manager to add the official HashiCorp Linux repository
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 
 # install terraform
-sudo yum -y install terraform
+yum -y install terraform
 
 # verify terraform is installed
 terraform --version
