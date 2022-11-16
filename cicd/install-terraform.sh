d130cb70b091c6ef0d96d05cc35563e0d9a51f66
mahdibenayed2 <47032423+mahdibenayed2@users.noreply.github.com>
2022-11-16 16:00:48 +0100

    Update install-terraform.sh

--- cicd/install-terraform.sh
+++ cicd/install-terraform.sh
@@ -6,6 +6,7 @@ set -eu
 # install yum-config-manager to manage your repositories
 yum install -y sudo
 sudo yum install -y yum-utils
+sudo sed 's/1/0/' /etc/yum/pluginconf.d/priorities.conf
 sudo cat /etc/yum/pluginconf.d/priorities.conf
 # use yum-config-manager to add the official HashiCorp Linux repository
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
